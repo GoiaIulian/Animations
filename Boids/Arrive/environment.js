32fc12d792ea0ab7ed3f24d698189f8b50d38c0d
GoiaIulian <goia.iulian@yahoo.com>
2019-05-19 17:59:10 +0300

    Boids/Arrive ++

---
+++ Boids/Arrive/environment.js
@@ -0,0 +1,39 @@
+let wasPressed = false, b;
+
+function setup()
+{
+     createCanvas(windowWidth, windowHeight);
+     b = new Boid();
+}
+
+function draw()
+{
+     background(255);
+     ellipse(mouseX, mouseY, 100);
+     let target = createVector(mouseX, mouseY);
+     // let enemy = createVector(mouseX, mouseY);
+     // b.seek(target);
+     // b.flee(enemy);
+     b.arrive(target);
+     b.update();
+     b.show();
+}
+
+// pause animation by pressing the mouse wheel
+// then press again to play
+function mousePressed()
+{
+     if (mouseButton === CENTER)
+     {
+          if (!wasPressed)
+          {
+               wasPressed = true;
+               noLoop();
+          }
+          else
+          {
+               wasPressed = false;
+               loop();
+          }
+     }
+}
